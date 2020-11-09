28eae9f69a43ac324a4da8e3f23463ac30c84c51
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-11-09 18:32:39 +0100

    🐛 FIX: remove unwanted api from gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,12 +2,6 @@
 import React from 'react'
 import Layout from './src/components/Layout'
 
-// Logs when the client route changes
-export const onRouteUpdate = ({ location, prevLocation }) => {
-  console.log('new pathname', location.pathname)
-  console.log('old pathname', prevLocation ? prevLocation.pathname : null)
-}
-
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>
 }
