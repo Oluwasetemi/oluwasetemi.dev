2ac0ad10ede9531567bae87eee60d440ca4ba1f5
oluwasetemi ojo <setemiojo@gmail.com>
2025-06-24 09:32:38 -0500

    improved the sorting order of the rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,30 @@
+// src/pages/rss.xml.js
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("blog", ({ data }) => data.date);
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map(post => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    items: posts
+      .filter(post => post.data.isPublished)
+      .sort((a, b) => {
+        return new Date(b.data.date) - new Date(a.data.date);
+      })
+      .map((post) => {
+        return {
+          ...post.data,
+          title: post.data.title,
+          link: `/blog/${post.slug}/`,
+          date: post.data.date || post.data.pubDate || post.data.publishDate,
+          description: post.data.description || post.data.excerpt || "",
+          categories: post.data.tags || [],
+        };
+      }),
   });
 }
